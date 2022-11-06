ab0089ae5310f120dd90f0f2b2d37efae18da9c1
RUBENXD713 <19170045@uttcampus.edu.mx>
2022-11-06 15:15:51 -0600

    ChangeOnDB

--- database/migrations/2022_11_06_061345_roles.php
+++ database/migrations/2022_11_06_061345_roles.php
@@ -17,6 +17,7 @@ class Roles extends Migration
             $table->id();
             $table->string('name');
             $table->string('description');
+            $table->bigInteger('status');
             $table->timestamps();
         });
     }
